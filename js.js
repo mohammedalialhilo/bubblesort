7c7f8d39c91d514aff9b7817d1eac12d2439f07f
Mohammed Ali <alhiloempire@gmail.com>
2019-09-23 14:51:33 +0200

    Co-authored-by: oliverjam29 <oliver.jam@elev.ga.ntig.se>

---
+++ js.js
@@ -0,0 +1,34 @@
+const listar = document.querySelector(".displayOutput");
+const sorterad = document.querySelector(".sortedList");
+const Maxtal = new Array();
+
+let lista = [Maxtal];
+while (Maxtal.length < 1000) {
+    Maxtal.push(Math.floor(Math.random() * 1000000) + 1);
+
+}
+//document.write(Maxtal);
+listar.innerHTML = Maxtal.toString();
+console.log(Maxtal);
+
+function swap(Maxtal, i, j) {
+    var temp = Maxtal[i];
+    Maxtal[i] = Maxtal[j];
+    Maxtal[j] = temp;
+}
+
+function bubbleSort(Maxtal) {
+    var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < Maxtal.length; i++) {
+            if (Maxtal[i] && Maxtal[i + 1] && Maxtal[i] > Maxtal[i + 1]) {
+                swap(Maxtal, i, i + 1);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return Maxtal;
+}
+
+console.log(bubbleSort(Maxtal.slice()));
